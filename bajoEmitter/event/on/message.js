a01df6e2ea8e944cbfab5f99c6e7eee606060577
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-05 20:55:07 +0700

    Updates

--- bajoEmitter/event/on/message.js
+++ bajoEmitter/event/on/message.js
@@ -1,5 +1,6 @@
 const onMessage = {
   handler: async function bajoMqttOnMessage (...args) {
+    // for broadcast only
     const { broadcast } = this.bajoEmitter.helper
     let [conn, topic, msg] = args
     for (const sub of this.bajoMqtt.subscribers) {
