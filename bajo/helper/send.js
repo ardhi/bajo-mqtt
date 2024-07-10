0627025f23fcea0ea5d1c622c3ab66f234b34560
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-10 09:49:14 +0700

    Update vars

--- bajo/helper/send.js
+++ bajo/helper/send.js
@@ -3,14 +3,14 @@ async function send ({ msg, from, to } = {}) {
   const { find, isEmpty } = this.app.bajo.helper._
   const { addressSplit } = this.app.bajoEmitter.helper
   const { publish } = this.helper
-  const { destination, connection, transport } = addressSplit(to)
-  if (transport !== 'bajoMqtt') return
-  if (isEmpty(destination)) return
+  const { subject, connection, plugin } = addressSplit(to)
+  if (plugin !== 'bajoMqtt') return
+  if (isEmpty(subject)) return
   const c = find(this.connections, { name: connection })
   if (!c) throw error('No such connection \'%s\'', connection)
   if (!c.instance) return
   if (!c.instance.connected) return
-  await publish(destination, msg, connection)
+  await publish(subject, msg, connection)
 }
 
 export default send
