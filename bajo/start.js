59d97b3262ef486d59f4a3ce3db34ba3440d87aa
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-21 07:27:04 +0700

    Refactor: bajo-emitter is now optional

--- bajo/start.js
+++ bajo/start.js
@@ -1,12 +1,15 @@
 import mqtt from 'mqtt'
 
 async function start () {
-  const { emit } = this.app.bajoEmitter
+  const { runHook } = this.app.bajo
+  const { camelCase } = this.app.bajo.lib._
+
   for (const c of this.connections ?? []) {
     const client = mqtt.connect(c.url, c.options)
     for (const evt of this.events) {
       client.on(evt, async (...args) => {
-        emit(`bajoMqtt.${evt}`, c, ...args)
+        if (this.app.bajoEmitter) this.app.bajoEmitter.emit(`${this.name}.${evt}`, c, ...args)
+        await runHook(`${this.name}:${camelCase('on ' + evt)}`, c, ...args)
       })
     }
     c.instance = client
